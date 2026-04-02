4a51b788e6816aa9d99948e37a18bc0d47a2eb9b
s1284642004 <1284642004@qq.com>
2026-04-03 01:17:20 +0800

    联通华为

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,13 +3,11 @@ plugins {
     alias(libs.plugins.kotlin.compose)
 }
 
+apply(plugin = "com.huawei.agconnect")
+
 android {
     namespace = "com.example.renthouseapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.renthouseapp"
@@ -17,7 +15,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -30,36 +27,45 @@ android {
             )
         }
     }
+
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true // 加上这一行
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
+    implementation(libs.agconnect.core)
+    implementation(libs.agconnect.cloud.database)
+
     implementation("androidx.compose.material3:material3:1.2.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,17 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://developer.huawei.com/repo/")
+        }
+    }
+    dependencies {
+        classpath("com.huawei.agconnect:agcp:1.9.5.302")
+    }
 }
